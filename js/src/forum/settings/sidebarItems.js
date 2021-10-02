46e5675d5b273df9f670af7c94152d991f176c29
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2021-10-02 16:37:30 +0300

    add settings button 3d view

--- js/src/forum/settings/sidebarItems.js
+++ js/src/forum/settings/sidebarItems.js
@@ -5,50 +5,60 @@ import SelectDropdown from 'flarum/components/SelectDropdown';
 
 export default function () {
 
-    extend(IndexPage.prototype, 'sidebarItems', function (items) {
-        if (items.has('nav')) {
-            items.remove('nav');
-        }
+  extend(IndexPage.prototype, 'sidebarItems', function (items) {
 
-        let items1 = this.navItems(this).toArray();
+    const vasiaSettings = JSON.parse(app.forum.attribute('block-cat.vasiaSettings'));
 
-        for (let i = 0; i < items1.length; i++) {
-            if (items1[i].itemName === "separator") {
-                items1.splice(i , items1.length --);
-            }
-        }
-        let items2 = this.navItems(this).toArray();
+    if (vasiaSettings.button_3d_new) {
+      let button_new = document.getElementsByClassName("item-newDiscussion")[0];
+      if (button_new != undefined) {
+        button_new.classList.add("item-newDiscussion_3d");
+      }
+    }
 
-        for (let i = 0; i < items2.length; i++) {
-            if (items2[i].itemName === "separator") {
-                items2.splice(0, i + 1);
-            }
-        }
-        
+    if (items.has('nav')) {
+      items.remove('nav');
+    }
 
-        items.add(
-            'nav_Navigation',
-            SelectDropdown.component(
-              {
-                buttonClassName: 'Button',
-                className: 'App-titleControl nav_Navigation',
-                accessibleToggleLabel: app.translator.trans('core.forum.index.toggle_sidenav_dropdown_accessible_label'),
-              },
-              items1
-            )
-          );
+    let items1 = this.navItems(this).toArray();
 
-          items.add(
-            'nav_Categories',
-            SelectDropdown.component(
-              {
-                buttonClassName: 'Button',
-                className: 'App-titleControl nav_Navigation',
-                accessibleToggleLabel: app.translator.trans('core.forum.index.toggle_sidenav_dropdown_accessible_label'),
-              },
-              items2
-            )
-          );
-    })
+    for (let i = 0; i < items1.length; i++) {
+      if (items1[i].itemName === "separator") {
+        items1.splice(i, items1.length--);
+      }
+    }
+    let items2 = this.navItems(this).toArray();
+
+    for (let i = 0; i < items2.length; i++) {
+      if (items2[i].itemName === "separator") {
+        items2.splice(0, i + 1);
+      }
+    }
+
+
+    items.add(
+      'nav_Navigation',
+      SelectDropdown.component(
+        {
+          buttonClassName: 'Button',
+          className: 'App-titleControl nav_Navigation',
+          accessibleToggleLabel: app.translator.trans('core.forum.index.toggle_sidenav_dropdown_accessible_label'),
+        },
+        items1
+      )
+    );
+
+    items.add(
+      'nav_Categories',
+      SelectDropdown.component(
+        {
+          buttonClassName: 'Button',
+          className: 'App-titleControl nav_Navigation',
+          accessibleToggleLabel: app.translator.trans('core.forum.index.toggle_sidenav_dropdown_accessible_label'),
+        },
+        items2
+      )
+    );
+  })
 
 }
