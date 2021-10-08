e3397a3c5dad34bbe8d3aed3fddb8f39cb9d1953
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2021-10-08 11:29:03 +0300

    button new discussion float right

--- js/src/forum/settings/ActionItems.js
+++ js/src/forum/settings/ActionItems.js
@@ -2,6 +2,7 @@ import app from 'flarum/common/app';
 import { extend, override } from 'flarum/extend';
 import IndexPage from 'flarum/components/IndexPage';
 import LinkButton from 'flarum/components/LinkButton';
+import Button from 'flarum/components/Button';
 
 export default function () {
 
@@ -33,9 +34,33 @@ export default function () {
         );
     })
 
+    extend(IndexPage.prototype, 'sidebarItems', function (items) {
+        if (items.has('newDiscussion')) {
+            items.remove('newDiscussion');
+        }
+    });
+
     extend(IndexPage.prototype, 'actionItems', function (items) {
 
-        
+        const canStartDiscussion = app.forum.attribute('canStartDiscussion') || !app.session.user;
+
+        items.add(
+            'newDiscussion',
+            Button.component(
+                {
+                    icon: 'fas fa-edit',
+                    className: 'Button Button--primary IndexPage-newDiscussion',
+                    itemClassName: 'App-primaryControl',
+                    onclick: () => {
+                        // If the user is not logged in, the promise rejects, and a login modal shows up.
+                        // Since that's already handled, we dont need to show an error message in the console.
+                        return this.newDiscussionAction().catch(() => { });
+                    },
+                    disabled: !canStartDiscussion,
+                },
+                app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button')
+            )
+        );
 
 
 
