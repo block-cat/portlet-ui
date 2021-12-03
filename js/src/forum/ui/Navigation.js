411dd5d409dff7e46a95115656c50aab15fcbe17
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2021-12-03 14:17:27 +0200

    1

--- js/src/forum/ui/Navigation.js
+++ js/src/forum/ui/Navigation.js
@@ -1,6 +1,7 @@
 import app from 'flarum/common/app';
 import { extend, override } from 'flarum/extend';
 import DiscussionPage from 'flarum/components/DiscussionPage';
+import PostsUserPage from 'flarum/components/PostsUserPage';
 import DiscussionListPane from 'flarum/components/DiscussionListPane';
 import LinkButton from 'flarum/components/LinkButton';
 import Button from 'flarum/components/Button';
@@ -81,6 +82,55 @@ export default function () {
             let span_pdf = document.getElementsByClassName("span_pdf");
             if (span_pdf != undefined) {
                 for (let i = 0; i < span_pdf.length; i++) {
+                    span_pdf[i].classList.add("d-none");
+                }
+            }
+            let iframe_pdf = document.getElementsByClassName("iframe_pdf");
+            if (iframe_pdf.length > 0) {
+                for (let i = 0; i < iframe_pdf.length; i++) {
+                    for (let j = 0; j < iframe_pdf[i].attributes.length; j++) {
+                        let text = iframe_pdf[i].innerHTML;
+
+                        var from = text.search('<a href="');
+                        from += 9;
+                        var to = text.length;
+                        var new_text = text.substring(from, to);
+
+                        var from2 = 0;
+                        var to2 = new_text.search('.pdf');
+                        to2 += 4;
+                        var new_text2 = new_text.substring(from2, to2);
+
+                        if (iframe_pdf[i].attributes[j].name == "src") {
+                            if (iframe_pdf[i].attributes[j].value == "")
+                                iframe_pdf[i].attributes[j].value = new_text2;
+                        }
+                    }
+                }
+            }
+        } else {
+            let iframe_pdf = document.getElementsByClassName("iframe_pdf");
+            if (iframe_pdf != undefined) {
+                for (let i = 0; i < iframe_pdf.length; i++) {
+                    iframe_pdf[i].classList.add("d-none");
+                }
+            }
+        }
+    });
+
+    extend(PostsUserPage.prototype, 'view', function (items) {
+
+        const vasiaSettings = JSON.parse(app.forum.attribute('block-cat.vasiaSettings'));
+        if (vasiaSettings.display_pdf_files) {
+            let span_pdf = document.getElementsByClassName("span_pdf");
+
+            if (span_pdf != undefined) {
+                let g = span_pdf;
+                console.log(g);
+                
+                for (let i = 0; i < span_pdf.length; i++) {
+                    console.log(span_pdf);
+
                     span_pdf[i].classList.add("d-none");
                 }
             }
